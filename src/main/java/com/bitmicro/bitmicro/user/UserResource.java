291559737b6d5e70e2bc8cfebd01f1f293019dac
HS00364364 <hareeshsirana1005@gmail.com>
2020-06-11 11:17:53 +0530

    exception handling and deleting service

--- src/main/java/com/bitmicro/bitmicro/user/UserResource.java
+++ src/main/java/com/bitmicro/bitmicro/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,12 @@ public class UserResource {
         	throw new UserNotFoundException("id-"+ id);
 		return user;
 	}
-	
+	@DeleteMapping("/users/{id}")
+	public void deleteById(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user==null)
+        	throw new UserNotFoundException("id-"+ id);
+	} 
 	@PostMapping("/users")
 	public ResponseEntity<Object> addUser(@RequestBody User user){
 		User savedUser = service.save(user);
